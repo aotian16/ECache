d3c7524996fddb2975208d962a5f7dcde953f222
tongjin <aotian16@gmail.com>
2016-07-01 15:13:31 +0800

    add ECacheUtil

---
+++ src/ECache/ecache/src/main/java/com/qefee/pj/ecache/util/ECacheUtil.java
@@ -0,0 +1,28 @@
+package com.qefee.pj.ecache.util;
+
+import android.content.Context;
+
+import com.qefee.pj.ecache.ECache;
+
+/**
+ * ECacheUtil.
+ * <ul>
+ * <li>date: 16/7/1</li>
+ * </ul>
+ *
+ * @author tongjin
+ */
+public class ECacheUtil {
+
+    private static ECache cache;
+
+    public static ECache getCache() {
+        return cache;
+    }
+
+    public static void init(Context context) {
+        cache = new ECache(context);
+    }
+
+
+}
